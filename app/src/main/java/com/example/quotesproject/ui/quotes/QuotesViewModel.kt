7e21c77667b85490e354eb299fc9e3d8511db787
Rohan Manjunath Ashlesh <rashlesh@deloitte.com>
2020-10-13 16:34:44 +0530

    define ViewModel class

--- app/src/main/java/com/example/quotesproject/ui/quotes/QuotesViewModel.kt
+++ app/src/main/java/com/example/quotesproject/ui/quotes/QuotesViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.quotesproject.ui.quotes
 
-class QuotesViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.quotesproject.data.Quote
+
+class QuotesViewModel(private val quoteRepository: QuoteRepository) : ViewModel() {
+
+    fun addQuotes(quote: Quote) = quoteRepository.addQuote(quote)
+
+    fun getQuote() = quoteRepository.getQuote()
 }
