4f92ed96eb59dc95ff560f97af8e18aa99dbb0d2
Rohan Manjunath Ashlesh <rashlesh@deloitte.com>
2020-10-13 15:32:25 +0530

    create repo function definitions

--- app/src/main/java/com/example/quotesproject/ui/quotes/QuoteRepository.kt
+++ app/src/main/java/com/example/quotesproject/ui/quotes/QuoteRepository.kt
@@ -1,4 +1,25 @@
 package com.example.quotesproject.ui.quotes
 
-class QuoteRepository {
+import com.example.quotesproject.data.FakeQuoteDao
+import com.example.quotesproject.data.Quote
+
+class QuoteRepository private constructor(private val quoteDao : FakeQuoteDao) {
+
+    fun addQuote(quote: Quote) {
+        quoteDao.addQuote(quote)
+    }
+
+    fun getQuote() = quoteDao.getQuote()
+
+    companion object {
+        // @Volatile - So that writes to this property are immediately visible to other threads
+        @Volatile
+        private var instance: QuoteRepository? = null
+
+        fun getInstance() = instance ?: synchronized(this) {
+            instance ?: QuoteRepository(quoteDao = FakeQuoteDao()).also { instance = it }
+        }
+
+    }
+
 }
