6c2686ee846c768f1599633eb5f6b06159e154b4
Rohan Manjunath Ashlesh <rashlesh@deloitte.com>
2020-10-13 16:36:07 +0530

    initialising ui in main activity

--- app/src/main/java/com/example/quotesproject/ui/quotes/MainActivity.kt
+++ app/src/main/java/com/example/quotesproject/ui/quotes/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.quotesproject.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.quotesproject.R
+import com.example.quotesproject.data.Quote
+import com.example.quotesproject.util.InjectorUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeUi()
+    }
+
+    private fun initializeUi() {
+        val factory = InjectorUtils.provideQuotesViewModelFactory()
+        val viewModel = ViewModelProvider(this, factory).get(QuotesViewModel::class.java)
+
+        viewModel.getQuote().observe(this, Observer { quotes ->
+            val stringBuilder = StringBuilder()
+            quotes.forEach { quote ->
+                stringBuilder.append("$quote\n\n")
+            }
+            textView_quotes.text = stringBuilder.toString()
+        })
+
+        button_add_quote.setOnClickListener {
+            val quote = Quote(editText_quote.text.toString(), editText_author.text.toString())
+            viewModel.addQuotes(quote)
+            editText_quote.setText("")
+            editText_author.setText("")
+        }
     }
 }
+
+
+
